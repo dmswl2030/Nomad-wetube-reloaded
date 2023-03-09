44e7156149878a00cd624fd121d36ef191c08052
EUNJI <eunji9180@naver.com>
2023-03-09 17:10:52 +0900

    230309 Wetube 배포 -4

--- src/server.js
+++ src/server.js
@@ -44,6 +44,7 @@ app.use((req, res, next) => {
 //   next();
 // });
 app.use("/uploads", express.static("uploads"));
+app.use("/videos/uploads", express.static("uploads"));
 app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
