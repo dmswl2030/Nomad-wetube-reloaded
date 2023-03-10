918fc056c41b9ed3337b933d2e5a2ec64c0be1ab
EUNJI <eunji9180@naver.com>
2023-03-10 20:32:00 +0900

    230310 배포 -23

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,17 +1,21 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, trim: true, maxLength: 80 },
-  fileUrl: { type: String },
-  thumbUrl: { type: String },
-  description: { type: String, trim: true, minLength: 20 },
-  createdAt: { type: Date, default: Date.now() },
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  fileUrl: { type: String, required: true },
+  thumbUrl: { type: String, required: true },
+  description: { type: String, required: true, trim: true, minLength: 20 },
+  createdAt: {
+    type: Number,
+    required: true,
+    default: new Date().getFullYear(),
+  },
   hashtags: [{ type: String, trim: true }],
   meta: {
-    views: { type: Number, default: 0 },
+    views: { type: Number, default: 0, required: true },
   },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
 videoSchema.static("formatHashtags", function (hashtags) {
