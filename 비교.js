9121391e418b2d83d3c8a5f1842705f44666e53b
EUNJI <eunji9180@naver.com>
2023-03-11 00:42:21 +0900

    230311 comment 수정 -2

--- 비교.js
+++ 비교.js
@@ -1,18 +1,16 @@
 import Video from "../models/Video";
 import Comment from "../models/Comment";
 import User from "../models/User";
-
 export const home = async (req, res) => {
   const videos = await Video.find({})
     .sort({ createdAt: "desc" })
     .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
-
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id).populate("owner").populate("comments");
-  const comments = await Comment.find({ video: id }).populate("owner"); //comment_owner
+  const comments = await Comment.find({ video: id }).populate("owner");
   console.log(comments.owner);
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
@@ -35,12 +33,11 @@ export const getEdit = async (req, res) => {
   }
   return res.render("edit", { pageTitle: `Edit ${video.title}`, video });
 };
-
 export const postEdit = async (req, res) => {
+  const { id } = req.params;
   const {
     user: { _id },
   } = req.session;
-  const { id } = req.params;
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
@@ -58,24 +55,21 @@ export const postEdit = async (req, res) => {
   req.flash("success", "Changes saved.");
   return res.redirect(`/videos/${id}`);
 };
-
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
-
 export const postUpload = async (req, res) => {
   const {
     user: { _id },
   } = req.session;
   const { video, thumb } = req.files;
   const { title, description, hashtags } = req.body;
-  const isHeroku = process.env.NODE_ENV === "production";
   try {
     const newVideo = await Video.create({
       title,
       description,
       fileUrl: isHeroku ? video[0].location : video[0].path,
-      thumbUrl: isHeroku ? thumb[0].location : thumb[0].path,
+      thumbUrl: isHeroku ? thumb[0].location : video[0].path,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
@@ -85,13 +79,12 @@ export const postUpload = async (req, res) => {
     return res.redirect("/");
   } catch (error) {
     console.log(error);
-    return res.status(400).render("upload", {
+    return res.status(404).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
   }
 };
-
 export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   const {
@@ -107,12 +100,11 @@ export const deleteVideo = async (req, res) => {
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
 };
-
 export const search = async (req, res) => {
   const { keyword } = req.query;
   let videos = [];
   if (keyword) {
-    videos = await Video.find({
+    const videos = await Video.find({
       title: {
         $regex: new RegExp(`${keyword}$`, "i"),
       },
@@ -120,7 +112,6 @@ export const search = async (req, res) => {
   }
   return res.render("search", { pageTitle: "Search", videos });
 };
-
 export const registerView = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
@@ -150,27 +141,24 @@ export const createComment = async (req, res) => {
     video: id,
   });
   const owner = await User.findById(comment.owner);
+
   owner.comments.push(comment._id);
   owner.save();
   video.comments.push(comment._id);
   video.save();
-  return res.status(201).json({ newCommentId: comment._id, owner });
+
+  return res.status(201).json({ newCommentId: comment._i, owner });
 };
 
 export const deleteComment = async (req, res) => {
+  const { id } = req.params;
   const {
     user: { _id },
   } = req.session;
-  const {
-    params: { id },
-  } = req;
-
-  const comment = await Comment.findByIdAndDelete(id);
+  const comment = await Comment.findById(id);
   if (!comment) {
     return res.sendStatus(404);
   }
-  if (String(comment.owner) !== String(_id)) {
-    return res.sendStatus(403);
-  }
-  return res.sendStatus(201);
+  await Comment.findByIdAndDelete(id);
+  return res.sendStatus(200);
 };
