4d24e8b00392589403223b768262b3585c19cade
EUNJI <eunji9180@naver.com>
2023-03-16 19:07:34 +0900

    230316 배포 후 수정: edit video 수정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -42,12 +42,12 @@ export const postEdit = async (req, res) => {
   } = req.session;
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  // const video = await Video.findById(id);
-  const video = await Video.exists({ _id: id });
+  const video = await Video.findById(id);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
+    console.log(error);
     req.flash("error", "You are not the the owner of the video.");
     return res.status(403).redirect("/");
   }
